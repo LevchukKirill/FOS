28f42688477af6c3ebb20430107e9384e066cb71
Kirill Levchuk <kirya.levch@mail.ru>
2024-03-13 19:18:46 +0200

    creating main pages

---
+++ screens/Busket/Busket.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Busket = () => {
+  return (
+    <View>
+      <Text>Busket</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Busket;
