6df5109245dcb13038658c415c57122838daa9d7
Kirill Levchuk <kirya.levch@mail.ru>
2024-03-17 19:43:31 +0200

    centeralize header (css fix)

--- components/layout/Header/Header.jsx
+++ components/layout/Header/Header.jsx
@@ -1,15 +1,27 @@
-import { Button, Linking, Text, View, StyleSheet } from "react-native";
+import {
+  Button,
+  Linking,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+} from "react-native";
 import Auth from "../Auth/Auth.jsx";
+import React from "react";
+import { COLORS } from "../../../constants";
 
 const Header = () => {
   return (
     <View style={styles.header}>
       <View>
-        <Button
-          title="BONUSI"
+        <Pressable
+          style={styles.authBtn}
           onPress={() => Linking.openURL("https://google.com")}
-        />
+        >
+          <Text>Binus</Text>
+        </Pressable>
       </View>
+
       <View>
         <Auth />
       </View>
@@ -19,12 +31,16 @@ const Header = () => {
 
 const styles = StyleSheet.create({
   header: {
-    borderColor: "black",
-    height: 60,
-    margin: "auto",
-    alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  authBtn: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    backgroundColor: COLORS.primary,
   },
 });
 
