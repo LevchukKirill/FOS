2af212cecd69cc2a3394574ab957ff9403381c9a
Kirill Levchuk <kirya.levch@mail.ru>
2024-03-29 16:51:54 +0200

    fix routes

--- server/routes/foodRouter.js
+++ server/routes/foodRouter.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const foodController = require('../controllers/foodController');
-const checkRole = require('../middleware/checkRoleMiddleware');
+const foodController = require("../controllers/foodController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post('/', checkRole('ADMIN'), foodController.create);
-router.get('/', foodController.getAll);
-router.get('/:id', foodController.getOne);
-router.put('/:id', checkRole('ADMIN'), foodController.update);
-router.delete('/:id', checkRole('ADMIN'), foodController.delete);
+// router.post('/', checkRole('ADMIN'), foodController.create);
+
+router.post("/", foodController.create);
+router.get("/", foodController.getAll);
+router.get("/:id", foodController.getOne);
+router.put("/:id", checkRole("ADMIN"), foodController.update);
+router.delete("/:id", checkRole("ADMIN"), foodController.delete);
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const foodRouter = require('./foodRouter');
-const orderRouter = require('./orderRouter');
-const typeRouter = require('./typeRouter');
-const userRouter = require('./userRouter');
-const infoRouter = require('./infoRouter');
-const restaurantRouter = require('./restaurantRouter');
+const foodRouter = require("./foodRouter");
+const orderRouter = require("./orderRouter");
+const typeRouter = require("./typeRouter");
+const userRouter = require("./userRouter");
+const infoRouter = require("./infoRouter");
+const restaurantRouter = require("./restaurantRouter");
+const orderFoodRouter = require("./orderFoodRouter");
 
-router.use('/user', userRouter);
-router.use('/type', typeRouter);
-router.use('/order', orderRouter);
-router.use('/food', foodRouter);
-router.use('/restaurant', restaurantRouter);
-router.use('/foodinfo', infoRouter);
+router.use("/user", userRouter);
+router.use("/type", typeRouter);
+router.use("/order", orderRouter);
+router.use("/orderfood", orderFoodRouter);
+router.use("/food", foodRouter);
+router.use("/restaurant", restaurantRouter);
+router.use("/foodinfo", infoRouter);
 
 module.exports = router;
